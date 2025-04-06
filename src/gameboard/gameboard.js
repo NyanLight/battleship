5f40e1c83ffa50ccc9e414e40635b7244ffa0629
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-04-06 23:55:01 +0400

    Add gameboard class with corresponding tests

---
+++ src/gameboard/gameboard.js
@@ -0,0 +1,49 @@
+import { Ship } from "../ship/ship";
+
+export class Gameboard {
+  constructor() {
+    this.board = this.createBoard();
+    this.misses = [];
+    this.heads = [];
+  }
+
+  createBoard() {
+    let board = [];
+    for (let i = 0; i < 10; i++) {
+      let row = [];
+      for (let j = 0; j < 10; j++) {
+        let cell = null;
+        row.push(cell);
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  placeShip(array) {
+    this.heads.push(array[0]);
+    const ship = new Ship(array.length);
+    array.forEach((coordinates) => {
+      const x = coordinates[0];
+      const y = coordinates[1];
+      this.board[x][y] = ship;
+    });
+  }
+
+  receiveAttack(x, y) {
+    if (this.board[x][y] === null) {
+      this.misses.push([x, y]);
+    } else {
+      this.board[x][y].hit();
+    }
+  }
+
+  allSunk() {
+    for (let i = 0; i < this.heads.length; i++) {
+      const x = this.heads[i][0];
+      const y = this.heads[i][1];
+      if (!this.board[x][y].isSunk()) return false;
+    }
+    return true;
+  }
+}
---
+++ src/gameboard/gameboard.test.js
@@ -0,0 +1,62 @@
+import { Gameboard } from "./gameboard.js";
+
+describe("allSunk method", () => {
+  const gb = new Gameboard();
+  gb.placeShip([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+  gb.placeShip([[2, 1]]);
+  gb.receiveAttack(0, 0);
+  gb.receiveAttack(0, 1);
+  gb.receiveAttack(0, 2);
+  it("Return false if there are still ships that aren't sunk", () => {
+    expect(gb.allSunk()).toBe(false);
+  });
+  it("Return true if all of the ships are sunk", () => {
+    gb.receiveAttack(2, 1);
+    expect(gb.allSunk()).toBe(true);
+  });
+});
+
+describe("receiveAttack misses", () => {
+  const gb = new Gameboard();
+  gb.placeShip([[2, 3]]);
+  gb.placeShip([
+    [3, 3],
+    [3, 4],
+  ]);
+  gb.receiveAttack(0, 1);
+  it("if enemy missed, gameboard records this", () => {
+    expect(
+      gb.misses.find(
+        (element) => JSON.stringify(element) === JSON.stringify([0, 1])
+      )
+    ).not.toBe(undefined);
+  });
+  gb.receiveAttack(2, 3);
+  it("if enemy hit the ship, gameboard doesn't record this to misses", () => {
+    expect(
+      gb.misses.find(
+        (element) => JSON.stringify(element) === JSON.stringify([2, 3])
+      )
+    ).toBe(undefined);
+  });
+});
+
+it("Heads actually works and store head of ships", () => {
+  const gb = new Gameboard();
+  gb.placeShip([
+    [2, 3],
+    [2, 4],
+    [2, 5],
+  ]);
+  gb.placeShip([
+    [1, 2],
+    [1, 3],
+  ]);
+  expect(gb.heads.length).toBe(2);
+  gb.placeShip([[0, 0]]);
+  expect(gb.heads.length).toBe(3);
+});
