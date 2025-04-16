0c0be7c317a896097c67622bcc8836010d8b2cac
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-04-16 16:36:32 +0400

    Format file and delete unused function, forbid player to attack certain coordinates

--- src/modules/dom/dom.js
+++ src/modules/dom/dom.js
@@ -1,34 +1,31 @@
+import { playerRound } from "../controller/controller";
+
 export function updateRender(player) {
   const fieldNodes = document.getElementsByClassName("gameboard");
-  const field = player.name !== "Computer" ? fieldNodes[0] : fieldNodes[1];
-  field.innerHTML = ''; 
+  const field = player.type !== "cpu" ? fieldNodes[0] : fieldNodes[1];
+  field.innerHTML = "";
   renderGameboard(player);
 }
 
 export function renderGameboard(player) {
   const fieldNodes = document.getElementsByClassName("gameboard");
-  const field = player.name !== "Computer" ? fieldNodes[0] : fieldNodes[1];
+  const field = player.type !== "cpu" ? fieldNodes[0] : fieldNodes[1];
   const gameboard = player.gameboard;
   for (let i = 0; i < gameboard.cells.length; i++) {
     for (let j = 0; j < gameboard.cells[i].length; j++) {
       const cell = document.createElement("div");
       cell.classList.toggle("cell");
-      if (gameboard.cells[i][j].status === 'missed') {
+      if (gameboard.cells[i][j].status === "missed") {
         cell.classList.toggle("miss");
-        cell.textContent = 'x';
-      } else if (gameboard.cells[i][j].status === 'hit') {
-        cell.classList.toggle('hit');
-        cell.textContent = 'X'
-      } else if (gameboard.cells[i][j].status === 'ship') {
-        cell.classList.toggle('ship');
+        cell.textContent = "x";
+      } else if (gameboard.cells[i][j].status === "hit") {
+        cell.classList.toggle("hit");
+        cell.textContent = "X";
+      } else if (gameboard.cells[i][j].status === "ship") {
+        cell.classList.toggle("ship");
       }
-      if (gameboard.cells[i][j].status === 'ship' || gameboard.cells[i][j].status === 'empty')   cell.addEventListener('click', () => attackCell(i, j, player));
+      if (player.type === "cpu" && (player.gameboard.cells[i][j].status === 'ship' || player.gameboard.cells[i][j].status === 'empty')) cell.addEventListener("click", () => playerRound(i, j));
       field.appendChild(cell);
     }
   }
-}
-
-export function attackCell(x, y, target) {
-  target.gameboard.receiveAttack(x, y);
-  updateRender(target);
 }
