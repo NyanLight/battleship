0b021ac88c682949ed5c68bcfb74be23cbfe7213
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-04-16 16:34:01 +0400

    Rewrite game controller to work in separate module, add players objects to module's scope
    Add players objects to module's scope to improve dom-related work.

---
+++ src/modules/controller/controller.js
@@ -0,0 +1,67 @@
+import { renderGameboard, updateRender } from "../dom/dom.js";
+import { Player } from "../player/player.js";
+
+export const player = new Player("player");
+export const cpu = new Player("cpu");
+
+function randomCoordinates() {
+  let coordinates = [];
+  for (let i = 0; i < 2; i++) {
+    coordinates.push(Math.floor(Math.random() * 10));
+  }
+  return coordinates;
+}
+
+function getCpuTurn() {
+  let [x, y] = randomCoordinates();
+  if (
+    player.gameboard.cells[x][y].status !== "ship" ||
+    player.gameboard.cells[x][y].status !== "empty"
+  )
+    [x, y] = randomCoordinates();
+  return [x, y];
+}
+
+function checkWin(pretender) {
+  const target = pretender === player ? cpu : player;
+  return target.gameboard.allSunk();
+}
+
+export function playerRound(x, y) {
+  cpu.gameboard.receiveAttack(x, y);
+  updateRender(cpu);
+  if (cpu.gameboard.cells[x][y].status !== "hit") {
+    computerRound(player);
+  } else {
+    checkWin(player);
+  }
+}
+
+function computerRound() {
+  setTimeout(() => {
+    const [x, y] = getCpuTurn();
+    player.gameboard.receiveAttack(x, y);
+    updateRender(player);
+    if (player.gameboard.cells[x][y].status !== "hit") {
+      console.log("missed");
+    } else if (checkWin(cpu)) {
+      alert('CPU is GOAT')
+    } 
+  }, 1000)
+}
+
+export function playGame() {
+  player.gameboard.placeShip([
+    [0, 0],
+    [0, 1],
+  ]);
+  cpu.gameboard.placeShip([
+    [0, 0],
+    [0, 1],
+  ]);
+
+  renderGameboard(player);
+  renderGameboard(cpu);
+
+  computerRound();
+}
