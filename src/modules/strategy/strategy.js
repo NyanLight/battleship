e69463435d2a363eb1e19ec99f1d128b91c235d4
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-04-30 19:20:40 +0400

    Fix bug in strategy

--- src/modules/strategy/strategy.js
+++ src/modules/strategy/strategy.js
@@ -7,15 +7,14 @@ export class Strategy {
 
   log(turn) {
     const [x, y] = turn;
-    if (player.gameboard.canReceiveAttackAt(x, y - 1)) {
+    if (player.gameboard.canReceiveAttackAt(x, y - 1))
       this.queque.push([x, y - 1]);
-    } else if (player.gameboard.canReceiveAttackAt(x - 1, y)) {
+    if (player.gameboard.canReceiveAttackAt(x - 1, y))
       this.queque.push([x - 1, y]);
-    } else if (player.gameboard.canReceiveAttackAt(x, y + 1)) {
+    if (player.gameboard.canReceiveAttackAt(x, y + 1))
       this.queque.push([x, y + 1]);
-    } else if (player.gameboard.canReceiveAttackAt(x + 1, y)) {
+    if (player.gameboard.canReceiveAttackAt(x + 1, y))
       this.queque.push([x + 1, y]);
-    }
   }
 
   clearLog() {
