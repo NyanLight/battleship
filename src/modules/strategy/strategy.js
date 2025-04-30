7d12216c6dd52f4a7942a2abe080991f7007670c
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-04-30 16:59:26 +0400

    Change strategy logic to be smarter, implement queque
    NowNow it is impossible for the computer to not finish off the ship and
    leave. Previously, this was possible if the computer hit the center of
    the ship with its first hit.

--- src/modules/strategy/strategy.js
+++ src/modules/strategy/strategy.js
@@ -2,34 +2,34 @@ import { getCpuTurn, player } from "../controller/controller";
 
 export class Strategy {
   constructor() {
-    this.lastHit = null;
+    this.queque = [];
   }
 
   log(turn) {
-    this.lastHit = turn;
+    const [x, y] = turn;
+    if (player.gameboard.canReceiveAttackAt(x, y - 1)) {
+      this.queque.push([x, y - 1]);
+    } else if (player.gameboard.canReceiveAttackAt(x - 1, y)) {
+      this.queque.push([x - 1, y]);
+    } else if (player.gameboard.canReceiveAttackAt(x, y + 1)) {
+      this.queque.push([x, y + 1]);
+    } else if (player.gameboard.canReceiveAttackAt(x + 1, y)) {
+      this.queque.push([x + 1, y]);
+    }
   }
 
   clearLog() {
-    this.turns = null;
+    this.queque = [];
   }
 
   chooseAttack() {
-    if (!this.lastHit) {
+    if (this.queque.length === 0) {
       const [x, y] = getCpuTurn();
       return [x, y];
     } else {
-      const [x, y] = this.lastHit;
-      if (player.gameboard.canReceiveAttackAt(x, y - 1)) {
-        return [x, y - 1];
-      } else if (player.gameboard.canReceiveAttackAt(x - 1, y)) {
-        return [x - 1, y];
-      } else if (player.gameboard.canReceiveAttackAt(x, y + 1)) {
-        return [x, y + 1];
-      } else if (player.gameboard.canReceiveAttackAt(x + 1, y)) {
-        return [x + 1, y];
-      } else {
-        return getCpuTurn();
-      }
+      const turn = this.queque[0];
+      this.queque.shift();
+      return turn;
     }
   }
 }
